d492ef5a6f8fdbede277451ee47a45dea62f0cba
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-09-03 10:37:00 +0530

    start javascript series

---
+++ DataTypes/Exercise.js
@@ -0,0 +1,24 @@
+//Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+
+var output=document.getElementById("output");
+
+let val="30 days of javascript";
+output.innerHTML=val;
+
+//Print the string on the browser console using console.log()
+
+console.log(val);
+
+//Length of the string:-
+
+console.log(val.length);
+
+//Change all the string characters to capital letters using toUpperCase() method
+
+let ans="Sanidhya Varshney";
+
+console.log(ans.toUpperCase());
+
+//Change all the string characters to lowercase letters using toLowerCase() method
+
+console.log(ans.toLowerCase());
