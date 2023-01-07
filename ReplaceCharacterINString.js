04f76968d57910c9bb3863f717f7380b01a150f3
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-07 17:12:59 +0530

    replace character

---
+++ ReplaceCharacterINString.js
@@ -0,0 +1,9 @@
+// program to replace a character of a string
+
+const string = 'Mr Red has a red house and a red car';
+
+// replace the characters
+const newText = string.replace('red', 'blue');
+
+// display the result
+console.log(newText);
