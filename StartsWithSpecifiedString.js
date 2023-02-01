fc41202243b2df59eff4baddbf590763644d5292
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-02-01 18:00:18 +0530

    starts with string

---
+++ StartsWithSpecifiedString.js
@@ -0,0 +1,7 @@
+//Write a JavaScript function to test whether a string starts with a specified string
+
+function startsWith(input, string) {
+    return input.indexOf(string) === 0;
+  }
+  console.log(startsWith('js string exercises', 'js'));
+  
