c818ffc67381926667d6eb762e9e2327f91b17a7
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-02-02 17:11:27 +0530

    list properties

---
+++ ListPropertiesOfObject.js
@@ -0,0 +1,16 @@
+//Write a JavaScript program to list the properties of a JavaScript object.
+
+function _keys(obj) 
+ {
+    if (!isObject(obj)) return [];
+    if (Object.keys) return Object.keys(obj);
+    var keys = [];
+    for (var key in obj) if (_.has(obj, key)) keys.push(key);
+    return keys;
+  }
+function isObject(obj) 
+{
+    var type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+  }
+console.log(_keys({red: "#FF0000", green: "#00FF00", white: "#FFFFFF"}));
